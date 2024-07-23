0f1e764ed8153aad5e3bf2a913dd3fe364324219
DanStaf <danstaf@mail.ru>
2024-07-23 14:54:30 +0300

    lesson_qty added in course serializer

--- online_learning/serializers.py
+++ online_learning/serializers.py
@@ -1,16 +1,22 @@
 # from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
 from online_learning.models import Course, Lesson
+from rest_framework import serializers
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_qty = serializers.SerializerMethodField(read_only=True)
+
+    def get_lessons_qty(self, obj):
+        return obj.lesson_set.count()
+
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'picture', 'owner']
+        fields = ['id', 'title', 'description', 'picture', 'owner', 'lessons_qty']
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ['id', 'title', 'description', 'picture', 'course', 'owner']
+        fields = ['id', 'title', 'description', 'picture', 'url', 'course', 'owner']
         # fields = "__all__"
