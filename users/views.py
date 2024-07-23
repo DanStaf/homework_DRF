bb70c789b0e4829e36c80df34c856e9ba9892edb
DanStaf <danstaf@mail.ru>
2024-07-24 00:28:28 +0300

    JWT auth added, User login/register added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User, Payment
@@ -12,6 +13,17 @@ from users.serializers import UserSerializer, PaymentSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserCreateApiView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        new_user = serializer.save(is_active=True)
+        new_user.set_password(new_user.password)
+        new_user.save()
 
 
 class PaymentListApiView(generics.ListAPIView):
