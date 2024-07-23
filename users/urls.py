3abcb7cd1c9fa57e7ccdae91dd3b7cbab5a64af8
DanStaf <danstaf@mail.ru>
2024-07-23 19:10:10 +0300

    filters added for Payments

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from rest_framework import routers
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentListApiView
 from users.apps import UsersConfig
+from django.urls import path
 
 app_name = UsersConfig.name
 
 router_user = routers.DefaultRouter()
 router_user.register("", UserViewSet, basename='user')
 
-urlpatterns = []
+urlpatterns = [
+    path('payments/', PaymentListApiView.as_view(), name='payment_list'),
+]
+
 urlpatterns += router_user.urls
